8db3b693572016f986639eb2e2976b5ab3766409
cenunix <caden.hargrave@gmail.com>
2023-07-04 20:31:03 -0700

    Chance firmware path

--- pd-mapper.c
+++ pd-mapper.c
@@ -194,7 +194,7 @@ static int pd_load_map(const char *file)
 }
 
 #ifndef ANDROID
-#define FIRMWARE_BASE	"/home/cenunix/repos/"
+#define FIRMWARE_BASE	"/run/current-system/firmware/"
 #else
 #define FIRMWARE_BASE	"/run/current-system/firmware/"
 #endif
